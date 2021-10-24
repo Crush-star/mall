b6cdf184be7fef2411896f2cdc3634d9fb22d9be
青儿 <1311462428@qq.com>
2021-10-24 16:42:15 +0800

    master

--- src/views/proFile/proFile.vue
+++ src/views/proFile/proFile.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
+    <el-skeleton :rows="6" animated />
     <h2>我的</h2>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </template>
 
